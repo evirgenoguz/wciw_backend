81725ecc6231d4d98796419dec0feddbc5a05025
Oguz Evirgen <evirgenoguz@gmail.com>
2022-03-03 17:09:34 +0300

    hata düzeltme

--- src/main/java/com/evirgenoguz/wciw/WciwApplication.java
+++ src/main/java/com/evirgenoguz/wciw/WciwApplication.java
@@ -2,12 +2,7 @@ package com.evirgenoguz.wciw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class WciwApplication {
@@ -16,13 +11,5 @@ public class WciwApplication {
 		SpringApplication.run(WciwApplication.class, args);
 	}
 	
-	@Bean
-    public Docket api() { 
-        return new Docket(DocumentationType.SWAGGER_2)  
-          .select()                                  
-          .apis(RequestHandlerSelectors.any())              
-          .paths(PathSelectors.any())                          
-          .build();                                           
-    }
 
 }
